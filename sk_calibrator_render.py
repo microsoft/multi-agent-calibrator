bd12054716191b31754a183fc3dbf05c69be9e8c
Xinsong Li <lixinso@gmail.com>
2025-03-23 17:23:26 -0700

    load sample2 into calibrator

--- sk_calibrator_render.py
+++ sk_calibrator_render.py
@@ -3,11 +3,30 @@
 from flask import Flask, jsonify, abort, render_template, request
 import json
 import os
+# Import the configuration loader
+from sk_calibrator_config import load_config
+# Load endpoints from YAML configuration
+config = load_config("sample_sk_orchestrator_config.yaml")
+azure_openai_endpoint = config.get("azureopenai_endpoint")
+azure_openai_model = config.get("azureopenai_model")
+
+group_chat_info = {
+    "group_chat_name": "Calibrator_Component_Chat",
+    "group_chat_description": "Calibrator Component Chat",
+    "group_chat_instruction": "You are a calibrator component agent. Your job is to assist the user with calibration tasks."
+}
+agent_list = [
+    {"agent_name": "agent1", "agent_description": "TODO", "agent_instruction": " TODO:  User Input: {user_input}  Chat History: {chat_history_input}  {{{{$history}}}}", "service_id": "agent1_service_id"},]
+plugin_list = [
+    {"plugin_class_name": "sample2_components.Scrum_Champ", "plugin_name": "Scrum_Champ"}]
+function_list = [{"function_class_name": "sample2_components.search", "description": "description of func1"},]
 
 
 # Load the multi-agent chat and convert it to JSON
 from sk_calibrator_object_loader import load_group_chat, decode_multi_agent, convert_multi_agent_to_json
-multi_chat = load_group_chat()
+from sk_calibrator_component_assembler import AssembleAgentGroupChat
+#multi_chat = load_group_chat()
+multi_chat = AssembleAgentGroupChat(group_chat_info, agent_list,plugin_list, function_list, azure_openai_endpoint = azure_openai_endpoint, azure_openai_model = "gpt-4o-mini-deploy")
 decode_multi_agent(multi_chat)
 multi_chat_json = convert_multi_agent_to_json(multi_chat)
 print(multi_chat_json)
