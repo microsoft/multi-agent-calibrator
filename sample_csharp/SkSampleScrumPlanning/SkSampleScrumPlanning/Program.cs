c4df2b4f56753a18c84165e67200b24e98eefacd
Xinsong Li <lixinso@gmail.com>
2025-04-06 16:53:38 -0700

    C# sample

---
+++ sample_csharp/SkSampleScrumPlanning/SkSampleScrumPlanning/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Text.Json.Serialization;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+using Azure.Identity;
+using SkSampleScrumPlanning;
+
+public class ScrumBoardPlugin
+{
+    [KernelFunction("get_team_capacity")]
+    [Description("Gets team capacity for current sprint")]
+    public async Task<int> GetLightsAsync()
+    {
+        return 30;
+    }
+}
+
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        // Load configuration from environment variables or user secrets.
+        Settings settings = new();
+
+        var builder = Kernel.CreateBuilder();
+        Kernel kernel = builder.AddAzureOpenAIChatCompletion(
+            settings.AzureOpenAI.ChatModelDeployment,
+            settings.AzureOpenAI.Endpoint,
+            new AzureCliCredential())
+            .Build(); ;
+
+        // Create a kernel with Azure OpenAI chat completion
+        var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+        // Add a plugin (the LightsPlugin class is defined below)
+        kernel.Plugins.AddFromType<ScrumBoardPlugin>("ScrumBoard");
+
+        // Enable planning
+        OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+        {
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
+
+        // Create a history store the conversation
+        var history = new ChatHistory();
+        history.AddUserMessage("Help me find the team capacity for current sprint");
+
+        // Get the response from the AI
+        var result = await chatCompletionService.GetChatMessageContentAsync(
+           history,
+           executionSettings: openAIPromptExecutionSettings,
+           kernel: kernel);
+
+        // Print the results
+        Console.WriteLine("Assistant > " + result);
+
+        // Add the message from the agent to the chat history
+        history.AddAssistantMessage(result.Content);
+    }
+}
---
+++ sample_csharp/SkSampleScrumPlanning/SkSampleScrumPlanning/Settings.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkSampleScrumPlanning
+{
+    public class Settings
+    {
+        private readonly IConfigurationRoot configRoot;
+
+        private AzureOpenAISettings azureOpenAI;
+        private OpenAISettings openAI;
+
+        public AzureOpenAISettings AzureOpenAI => this.azureOpenAI ??= this.GetSettings<Settings.AzureOpenAISettings>();
+        public OpenAISettings OpenAI => this.openAI ??= this.GetSettings<Settings.OpenAISettings>();
+
+        public class OpenAISettings
+        {
+            public string ChatModel { get; set; } = string.Empty;
+            public string ApiKey { get; set; } = string.Empty;
+        }
+
+        public class AzureOpenAISettings
+        {
+            public string ChatModelDeployment { get; set; } = string.Empty;
+            public string Endpoint { get; set; } = string.Empty;
+            public string ApiKey { get; set; } = string.Empty;
+        }
+
+        public TSettings GetSettings<TSettings>() =>
+            this.configRoot.GetRequiredSection(typeof(TSettings).Name).Get<TSettings>()!;
+
+        public Settings()
+        {
+            this.configRoot =
+                new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets(Assembly.GetExecutingAssembly(), optional: true)
+                    .Build();
+        }
+    }
+
+}
